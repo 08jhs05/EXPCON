e699e5734f89ca749f86eecd1bc469611ade8b17
08jhs05 <08jhs05@gmail.com>
2021-07-29 02:21:42 -0400

    add scrollposition state that tracks current scroll position of window

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import Landing from './components/Landing';
 import Navigation from './components/Navigation';
+import Pricing from './components/Pricing'
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+
+//All styling is done in App.css
 import './App.css';
 
 function App() {
+    //this part gets current scroll position of window using usestate hook.
+    const [scrollPosition, setOffset] = useState(0);
+
+    useEffect(() => {
+        window.onscroll = () => {
+        setOffset(window.pageYOffset)
+        }
+    }, []);
+
   return (
     <div className="App">
         <Navigation />
-        <Landing />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Landing/>
+                </Route>
+                <Route path="/pricing">
+                    <Pricing/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
     </div>
   );
 }
